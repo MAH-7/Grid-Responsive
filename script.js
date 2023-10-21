98ad99834fa30a931f09907c710e7da1c31110c3
emanhakim <97828413+emanhakim@users.noreply.github.com>
2023-10-21 13:49:25 +0800

    minor fixs, change HoverMe text to ClickMe when mobileview

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+  // Function to update the text based on the viewport width
+  function updateText() {
+    if ($(window).width() <= 960) {
+      $(".change-text").text("ClickMe");
+    } else {
+      $(".change-text").text("HoverMe");
+    }
+  }
+
+  // Initial text update
+  updateText();
+
+  // Update text on window resize
+  $(window).on("resize", updateText);
+
   // Toggle the hover effect class when a card is clicked
   $(".card").on("click", function () {
     $(this).toggleClass("card-clicked");
